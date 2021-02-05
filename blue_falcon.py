a70915dfdb6a0f1381c1d0d05ea4f3ef55752f9e
BurningShed <BurningShed@gmail.com>
2021-02-04 19:38:28 -0500

    Updated Readme, deleted old test files, conda stuf

--- blue_falcon.py
+++ blue_falcon.py
@@ -15,15 +15,21 @@ logging.basicConfig(level=logging.DEBUG)
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-conf_filename = "config.toml"
 
 # Install the Slack app and get xoxb- token in advance
 app = App(
     token=os.environ["SLACK_OAUTH"]
 )
 
+# non-sensitive configuration information stored in config.toml
+conf_filename = "config.toml"
+
+
 def load_welcome_config():
-    """ loads the welcome message and channel from config.toml"""
+    """
+    loads the welcome message and channel from conf_filename
+    can be easily expanded to load other config data
+    """
     with open(conf_filename, 'r') as conf_file:
         config = tomlkit.loads(conf_file.read())
     return config["welcome"]
@@ -51,11 +57,14 @@ def greeter(ack, say, body, event):
     message_public = welcome_config["header"].format(**replacement_dict)
     message_reply = welcome_config["fold"].format(**replacement_dict)
 
+    # get thread_ts from first say in order to send second message as a thread
     thread_ts = say(text=message_public, channel=channel_id)["ts"]
     say(text=message_reply, channel=channel_id, thread_ts=thread_ts)
 
 
 welcome_config = load_welcome_config()
+
+# connects, joins the designated welcome channel, and starts running
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).connect()
 
